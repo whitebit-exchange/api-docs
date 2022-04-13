7d241eff95a5af69e1cc00586dd605d3c301d805
whitebit-robot <robot@whitebit.com>
2022-04-13 17:22:25 +0300

    deployment fix (#79)
    * refactor: metadata migrated. fixed links in readme
    
    * build: delete github action
    
    * build: fix
    
    Co-authored-by: Vladyslav Hryhoriev <snoopik.snoop@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'WhiteBIT API Documentation',
   tagline: '',
   url: 'https://whitebit-exchange.github.io',
-  baseUrl: '/',
+  baseUrl: '/api-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
