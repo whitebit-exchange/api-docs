854846ea3b54a0fd81fa1dac8878d0716e7e374b
whitebit-robot <robot@whitebit.com>
2022-05-04 23:48:16 +0300

    style fixes (#85)
    * refactor: metadata migrated. fixed links in readme
    
    * build: delete github action
    
    * build: fox
    
    * refactor: remove trade-v1 from sidebar
    
    * chore: seo updates
    
    * style: fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,41 +1,35 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'WhiteBIT API Documentation',
-  url: 'https://whitebit-exchange.github.io',
-  baseUrl: '/api-docs/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'whitebit-exchange',
-  projectName: 'api-docs',
+  title: "WhiteBIT API Documentation",
+  url: "https://whitebit-exchange.github.io",
+  baseUrl: "/api-docs/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "whitebit-exchange",
+  projectName: "api-docs",
   trailingSlash: false,
-  plugins: [
-    [
-      require.resolve("@cmfcmf/docusaurus-search-local"),
-      {
-      },
-    ],
-  ],
+  plugins: [[require.resolve("@cmfcmf/docusaurus-search-local"), {}]],
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         sitemap: {
-          changefreq: 'weekly',
+          changefreq: "weekly",
           priority: 0.5,
         },
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -43,27 +37,32 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      metadata: [{name: 'keywords', content: 'whitebit, api, docs, documentation'}],
+      metadata: [
+        { name: "keywords", content: "whitebit, api, docs, documentation" },
+      ],
       colorMode: {
-        defaultMode: 'dark',
+        defaultMode: "dark",
         respectPrefersColorScheme: true,
       },
+      image: "img/og-image.png",
       navbar: {
         logo: {
-          alt: 'WhiteBIT logo',
-          src: 'img/logo.svg',
+          alt: "WhiteBIT logo",
+          src: "img/logo.svg",
+          width: 32,
+          height: 32,
         },
         items: [
           {
-            type: 'doc',
-            docId: '/category/public',
-            position: 'left',
-            label: 'WhiteBIT API Docs',
+            type: "doc",
+            docId: "/category/public",
+            position: "left",
+            label: "WhiteBIT API Docs",
           },
           {
-            href: 'https://github.com/whitebit-exchange/api-docs',
-            className: 'navbar-item-github',
-            position: 'right',
+            href: "https://github.com/whitebit-exchange/api-docs",
+            className: "navbar-item-github",
+            position: "right",
           },
         ],
       },
