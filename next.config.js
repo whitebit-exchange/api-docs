510e2894c5be47b6b655093b0d3fea74c26b9a9f
whitebit-robot <robot@whitebit.com>
2023-01-16 10:58:12 +0100

    fix: path

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withNextra = require("nextra")({
 });
 
 const isProduction = process.env.NODE_ENV === "production";
-const assetPrefix = isProduction ? "/reading-notes" : "";
+const assetPrefix = isProduction ? "/api-documentation" : "";
 
 module.exports = {
   ...withNextra(),
