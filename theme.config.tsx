602840ebd76f86394a8459ec967fced67cbff126
whitebit-robot <robot@whitebit.com>
2023-01-16 11:26:45 +0100

    fix: logo paths

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,29 +1,35 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import { assetPrefix } from "./constants";
 
 const config: DocsThemeConfig = {
-  logo: <img src={"/static/img/logo.svg"} alt={"WhiteBIT logo"} width={32} height={32}/>,
+  logo: (
+    <img
+      src={`${assetPrefix}/img/logo.svg`}
+      alt={"WhiteBIT logo"}
+      width={32}
+      height={32}
+    />
+  ),
   project: {
-    link: 'https://github.com/whitebit-exchange/api-documentation',
+    link: "https://github.com/whitebit-exchange/api-documentation",
   },
-  docsRepositoryBase: 'https://github.com/whitebit-exchange/api-documentation',
+  docsRepositoryBase: "https://github.com/whitebit-exchange/api-documentation",
   useNextSeoProps() {
     return {
-      titleTemplate: '%s | WhiteBIT API Documentation',
+      titleTemplate: "%s | WhiteBIT API Documentation",
       openGraph: {
-        images: [
-          { url: '/static/img/og-image.png' },
-        ],
-        siteName: 'SiteName',
-      }
-    }
+        images: [{ url: `${assetPrefix}/img/og-image.png` }],
+        siteName: "WhiteBIT API Documentation",
+      },
+    };
   },
   primaryHue: 43,
   head: (
     <>
-      <link rel="icon" href="/static/img/favicon.svg" />
+      <link rel="icon" href={`${assetPrefix}/img/favicon.svg`} />
     </>
   ),
-}
+};
 
-export default config
+export default config;
