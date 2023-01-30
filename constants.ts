b721ec57a747d584dd26e5c8f8c374714e182201
whitebit-robot <robot@whitebit.com>
2023-01-30 10:06:45 +0100

    fix: assets url

--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
 const isProduction = process.env.NODE_ENV === "production";
-export const assetPrefix = isProduction ? "/api-documentation/static" : "/static";
+export const assetPrefix = isProduction ? "/api-docs/static" : "/static";
