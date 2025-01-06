1d576924318ff76f6ab93aaa131434085de112fd
whitebit-robot <robot@whitebit.com>
2025-01-06 18:45:39 +0100

    Refactor global styles and enhance font integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import "../global.css";
-
 import type { AppProps } from "next/app";
+import { Inter, Manrope } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
+});
+
+const manrope = Manrope({
+  subsets: ['latin'],
+  variable: '--font-manrope',
+  display: 'swap',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${inter.variable} ${manrope.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
